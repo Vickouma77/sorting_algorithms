445f3708f06fd8b78f9275275103004090a8868a
vickouma77 <vickprestone20@gmail.com>
2023-01-22 05:29:33 -0800

    updating sort header file

--- sort.h
+++ sort.h
@@ -14,3 +14,10 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif
