b6c859a122861ba1e0d863d088a4eb49f61ce6bb
vickouma77 <vickprestone20@gmail.com>
2023-01-24 01:44:52 -0800

    adding selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+#include <stdio.h>
+
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: The array
+ * @size: The size of the array
+ *
+ * Return: void
+ */
