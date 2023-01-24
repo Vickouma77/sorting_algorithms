95cd2562ad6dc2e6a4d663ab95f0e2e07d545368
vickouma77 <vickprestone20@gmail.com>
2023-01-24 01:19:41 -0800

    adding insertion list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * insertion_sort_list - sorts a DLL of integers in
+ * ascending order using the insertion sort
+ * algorithm
+ *
+ * @list: doubly linked list
+ * Return: no return
+ */
+
