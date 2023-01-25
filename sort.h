3735416378fc3f134f35bb568fb78f0ce5354d62
vickouma77 <vickprestone20@gmail.com>
2023-01-25 11:23:35 -0800

    updating sort header file

--- sort.h
+++ sort.h
@@ -33,4 +33,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+/*Advanced task Functions */
+
+void shell_sort(int *array, size_t size);
+
 #endif /* SORT_H */
